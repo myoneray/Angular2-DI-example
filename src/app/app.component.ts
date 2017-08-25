1759edf65d378f11799ecde7035d497917d9ca07
myoneray <myoneray@gmail.com>
2017-08-25 16:02:13 +0800

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
+//N03导入  MyService
+import {MyService} from './service/name.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+
+  title = '使用NgModule提供依赖';
+  content = '输出wangming即为成功注入！注意：providers: [MyService], 与 providers: [{ provide: MyService, useClass: MyService }]  等价!!';
+
+  //N04 将 MyService 注入构造函数
+  constructor(private myService: MyService) {
+    //N05使用它的实例
+    console.log(myService.getValue())
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
+//N01 导入MyService
+import { MyService} from './service/name.service';
 @NgModule({
   declarations: [
     AppComponent
@@ -14,7 +16,9 @@ import { AppComponent } from './app.component';
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  //N02 MyService添加进依赖中去
+  // providers: [MyService],
+  providers: [{ provide: MyService, useClass: MyService }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
